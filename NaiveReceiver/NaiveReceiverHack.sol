c45b4d4a511b66ca716d1f13b020f2d99275aeb0
Charlie Benson <34482986+CharlieJRBenson@users.noreply.github.com>
2023-03-15 13:42:45 +0000

    namechange

--- NaiveReceiver/NaiveReceiverHack.sol
+++ NaiveReceiver/NaiveReceiverHack.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.0;
 
 import "./NaiveReceiverLenderPool.sol";
 
-contract NaiveAttacker {
+contract NaiveReceiverHack {
     constructor(
         address payable poolAddress,
         address receiverAddress,
